12902a0cf8727b866543a294d53fdcc3bcb0c5ee
Christopher Hart <chart2@cisco.com>
2025-04-08 19:21:42 +0000

    chore: convert to using a YAML-based test plan, thus potentially reinventing Genie Harness

--- workspace/runner.py
+++ workspace/runner.py
@@ -2,7 +2,9 @@
 
 import argparse
 import logging
+from pathlib import Path
 
+import yaml
 from pyats.easypy import run
 from utils.types import RunningMode
 
@@ -22,6 +24,12 @@ parser.add_argument(
         "(validate against test case parameters)"
     ),
 )
+parser.add_argument(
+    "--test-plan",
+    type=str,
+    default="test_plan.yaml",
+    help="Path to the test plan YAML file",
+)
 
 
 def main(runtime):
@@ -36,12 +44,36 @@ def main(runtime):
 
     logger.info(f"Running in {str(args.mode)} mode")
 
-    # Execute test script with selected mode
-    run(
-        testscript="workspace/jobfiles/verify_iosxe_ospf_ipv4_neighbors_status.py",
-        runtime=runtime,
-        mode=args.mode,
-    )
+    # Validate provided test plan path exists
+    raw_test_plan_path = args.test_plan
+    test_plan_path = Path(raw_test_plan_path).resolve()
+    logger.info("Test plan filepath provided is '%s'", test_plan_path)
+    if not test_plan_path.exists():
+        logger.error("Test plan file does not exist: %s", test_plan_path)
+        raise FileNotFoundError(f"Test plan file does not exist: {test_plan_path}")
+
+    # Load test plan from YAML file
+    with open(test_plan_path, "r") as f:
+        test_plan = yaml.safe_load(f)
+
+    base_jobfile_directory = Path(test_plan["jobfile_directory"]).resolve()
+
+    for test_case_identifier, test_case_data in test_plan["test_cases"].items():
+        # Construct task ID
+        task_id = f"{test_case_identifier} - {test_case_data['title']}"
+        # Construct full jobfile path relative to the base jobfile directory
+        jobfile_path = base_jobfile_directory / test_case_data["jobfile"]
+
+        logger.info(
+            "Executing test case '%s' tied to jobfile at '%s'", task_id, jobfile_path
+        )
+
+        run(
+            testscript=str(jobfile_path),
+            runtime=runtime,
+            mode=args.mode,
+            task_id=task_id,
+        )
 
 
 if __name__ == "__main__":
