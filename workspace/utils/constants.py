2586b2f666025dbd0eb7d0135c9a5cda3289b401
Christopher Hart <chart2@cisco.com>
2025-04-11 16:08:18 +0000

    chore: add result and report-related constants

--- workspace/utils/constants.py
+++ workspace/utils/constants.py
@@ -5,3 +5,18 @@ from pathlib import Path
 # Save all parameters under the `parameters` directory at the root of the
 # project.
 PARAMETERS_DIR = Path(__file__).parent.parent.parent / "parameters"
+
+# Customer-facing results directories
+TEST_RESULTS_DIR = Path(__file__).parent.parent.parent / "test_results"
+REPORT_DIR = Path(__file__).parent.parent.parent / "test_report"
+REPORT_ASSETS_DIR = REPORT_DIR / "assets"
+REPORT_RESULTS_DIR = REPORT_DIR / "results"
+AGGREGATED_REPORT_FILENAME = "test_results_summary.html"
+
+# Template names for job files
+TEMPLATE_NAMES = {
+    "description": "DESCRIPTION",
+    "setup": "SETUP",
+    "procedure": "PROCEDURE",
+    "pass_fail_criteria": "PASS_FAIL_CRITERIA",
+}
